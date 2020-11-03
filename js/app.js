e4311f7391762fa8514215e9c84a1260238c513c
‘hasankhalifeh’ <hasan_khalifeh@Hotmail.com>
2020-11-03 13:30:51 +0200

    Final Exam

---
+++ js/app.js
@@ -0,0 +1,35 @@
+'user strict'
+
+// cateogry[
+//    "history",
+//    "Comedey",
+//    "Horror",
+//    "Romance",
+//    "Supernatural",
+
+// ]
+
+
+
+function generate_table(){
+    var result = document.getElementById("table1");
+
+    var tbl= document.createElement("table1");
+    var tblbody = document.createElement("tbody");
+
+    for (var i=0; i< 4;i++){
+        
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+console.log(getRandomInt(8));
+
+
+sendOrderLocalStorage();
+Function sendOrderLocalStorage('Anime titles');
+var animeTitles = JSON.stringify('anime');
+
+localStorage.setItem('anime');
